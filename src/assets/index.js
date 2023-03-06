4d5cb174d50fd1ad8c5da7e2b3ecd5f0e66f64c7
sajib925 <sajibahmed1004@gmail.com>
2023-03-07 00:01:48 +0600

    some update

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,7 @@ import bannerImg from "./images/hero/me-2.png";
 import projectOne from "./images/projects/baby-store.png";
 import projectTwo from "./images/projects/kacha-bazar.png";
 import projectThree from "./images/projects/grover.png";
-import projectFour from "./images/projects/projectOne.jpg";
+import projectFour from "./images/projects/admin-dashboard.png";
 import projectFive from "./images/projects/okiro.png";
 import projectSix from "./images/projects/pungent.png";
 import quote from "./images/testmonial/quote.png";
