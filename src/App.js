f689bf622142c84ec6970eee6b7846a0d1109ee2
sajib925 <sajibahmed1004@gmail.com>
2023-04-01 19:31:20 +0600

    some update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Banner from "./components/banner/Banner";
 import Contact from "./components/contact/Contact";
 import Features from "./components/features/Features";
@@ -7,8 +7,22 @@ import FooterBottom from "./components/footer/FooterBottom";
 import Navbar from "./components/navbar/Navbar";
 import Projects from "./components/Projects/Projects";
 import Resume from "./components/resume/Resume";
+import {BsArrow90DegUp} from "react-icons/bs";
 
 function App() {
+  const [isScroll, setIsScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 300) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+  });
+
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText">
       <Navbar />
@@ -20,6 +34,19 @@ function App() {
         <Contact />
         <Footer />
         <FooterBottom />
+
+        <div
+          className={`scrollIcon duration-500 ease-in-out ${
+            isScroll ? "right-5" : "-right-[100%]"
+          } `}
+        >
+          <a
+            href="#home"
+            className="w-[100%] h-[100%] flex items-center justify-center"
+          >
+            <BsArrow90DegUp size={22} />
+          </a>
+        </div>
       </div>
     </div>
   );
